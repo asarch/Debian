988c76f22bce32a06cb6ada37fb1be5e8230cbf6
asarch <asarch@hotmail.com>
2021-08-21 17:32:40 -0500

    Updated for Bullseye

--- list.sh
+++ list.sh
@@ -3,7 +3,8 @@
 # Packages that I don't need but I want to have them
 
 # Kernel
-apt-get -t buster-backports install linux-image-amd64
+#apt-get -t buster-backports install linux-image-amd64
+apt-get -t bullseye-backports install linux-image-amd64
 
 # Firmware for ellis
 apt-get install intel-microcode firmware-realtek firmware-misc-nonfree firmware-atheros
@@ -11,25 +12,54 @@ apt-get install intel-microcode firmware-realtek firmware-misc-nonfree firmware-
 # Firmware for paola/fabiola
 apt-get install amd64-microcode firmware-realtek firmware-amd-graphics firmware-atheros
 
+# Firmware for pasifae
+# Faltaría el siguiente firmware:
+# [   11.058662] bluetooth hci0: firmware: failed to load brcm/BCM20702A1-0a5c-21f1.hcd (-2)
+# [   11.058718] firmware_class: See https://wiki.debian.org/Firmware for information about missing firmware
+# [   11.058797] bluetooth hci0: firmware: failed to load brcm/BCM-0a5c-21f1.hcd (-2)
+# [   11.058841] Bluetooth: hci0: BCM: firmware Patch file not found, tried:
+#
+# Checa la página sugerida para más información
+apt-get install intel-microcode firmware-misc-nonfree firmware-realtek firmware-b43-installer
+
 # Tools
-apt-get install build-essential virt-manager qemu-utils bridge-utils ssh-askpass-gnome mc git hyperspec lynx blender anjuta anjuta-extras bluefish bluefish-plugins geany geany-plugins gummi xmlcopyeditor glade texinfo groff dblatex htmldoc sbcl sbcl-doc sbcl-source abiword gnucash gnumeric audacious audacious-plugins mplayer ffmpeg texinfo gnumeric-plugins-extra sox libsox-fmt-all vorbis-tools mpg123 inkscape inkscape-open-symbols inkscape-tutorials maxima maxima-doc wxmaxima gnome-builder apt-file stella freeglut3 freeglut3-dev virt-manager qemu qemu-kvm aptitude emacs emacs-common-non-dfsg vim vim-gtk3 vim-addon-manager vim-scripts hexchat hexchat-plugins nasm sasm libsdl1.2-dev togl-demos rtorrent tmux amule amule-utils-gui squeak-vm etoys etoys-doc sugar-etoys-activity jigdo-file swi-prolog swi-prolog-doc swi-prolog-bdb swi-prolog-x curl ntpdate libcanberra-gtk-module tree gimp gimp-gutenprint dia dia2code geeqie libgtk-3-doc libgtk2.0-doc libgtk2-perl-doc libgtkmm-2.4-doc libgtkmm-3.0-doc docbook-xsl-ns seed-webkit2 seed-webkit2-doc
+apt-get install build-essential virt-manager qemu-utils bridge-utils ssh-askpass-gnome mc git hyperspec lynx blender anjuta anjuta-extras bluefish bluefish-plugins geany geany-plugins gummi xmlcopyeditor glade texinfo groff dblatex htmldoc  abiword gnucash gnumeric audacious audacious-plugins mplayer ffmpeg texinfo gnumeric-plugins-extra sox libsox-fmt-all vorbis-tools mpg123 inkscape inkscape-open-symbols inkscape-tutorials maxima maxima-doc wxmaxima gnome-builder apt-file stella freeglut3 freeglut3-dev virt-manager qemu aptitude emacs emacs-common-non-dfsg vim vim-gtk3 vim-addon-manager vim-scripts hexchat hexchat-plugins nasm sasm libsdl1.2-dev togl-demos rtorrent tmux amule amule-utils-gui squeak-vm etoys etoys-doc sugar-etoys-activity jigdo-file swi-prolog swi-prolog-doc swi-prolog-bdb swi-prolog-x curl ntpdate libcanberra-gtk-module tree gimp gimp-gutenprint dia dia2code geeqie libgtk-3-doc libgtk2.0-doc libgtkmm-2.4-doc libgtkmm-3.0-doc docbook-xsl-ns 
+
+# GNU Smalltalk
+apt-get install gnu-smalltalk gnu-smalltalk-browser gnu-smalltalk-doc
+
+# SBCL
+# Solamente necesitas el binario para compilar la nueva versión
+apt-get install sbcl #sbcl-doc sbcl-source
+
+# Obsoletos en bullseye
+#qemu-kvm libgtk2-perl-doc seed-webkit2 seed-webkit2-doc
 
 # Xen
 # To fix permissions: user=foo usermod -G vboxusers,libvirt,libvirt-qemu,libvirt-dnsmasq$( groups $user | awk -F: '{ print $2 }' | sed 's/ /,/g' ) $user
+# E.g: usermod -G libvirt,libvirt-qemu$( groups asarch | awk -F: '{ print $2 }' | sed 's/ /,/g' ) asarch
 apt-get install xen-linux-system xen-utils bridge-utils virt-manager
 
 # ReiserFS
 apt-get install reiserfsprogs # ReiserFS 3.x
-apt-get install reiser4progs  # ReiserFS 4.x
+
+# Puedes formatear pero no usar, tienes que parchar al kernel
+apt-get install reiser4progs  # ReiserFS 4.x: 
 
 # Update database
 apt-file update
 
 # PostgreSQL
-PG_VER=13 apt-get install postgresql-$PG_VER postgresql-client-$PG_VER postgresql-doc-$PG_VER postgresql-server-dev-$PG_VER
+export PG_VER=13
+apt-get install postgresql-$PG_VER postgresql-client-$PG_VER postgresql-doc-$PG_VER postgresql-server-dev-$PG_VER
 
-# Python 3 venv (python3 -m venv alpha && source ~/alpha/bin/activate && pip --verbose install tg.devtools flask sqlacodegen psycopg2 django django-extensions)
+# Python 3 venv 
+#
+# python3 -m venv alpha
+# source ~/alpha/bin/activate
+# pip --verbose install tg.devtools flask sqlacodegen psycopg2 django django-extensions
 # deactivate
+#
 apt-get install python3-dev python3-venv
 
 # Python 3 SQLAlchemy
@@ -42,7 +72,10 @@ apt-get install libgtk2.0-dev libgtk2.0-doc libreadline-dev freeglut3-dev cmake 
 apt-get install nodejs nodejs-doc npm node-express node-express-generator
 
 # LibreOffice Base
-apt-get install libreoffice-base libreoffice-base-drivers libreoffice-evolution libreoffice-report-builder libreoffice-script-provider-python libreoffice-script-provider-js libreoffice-sdbc-hsqldb libreoffice-sdbc-postgresql libreoffice-mysql-connector libreoffice-pdfimport
+apt-get install libreoffice-base libreoffice-base-drivers libreoffice-evolution libreoffice-report-builder libreoffice-script-provider-python libreoffice-script-provider-js libreoffice-sdbc-hsqldb libreoffice-sdbc-postgresql libreoffice-mysql-connector
+
+# Paquetes obsoletos con bullseye
+# libreoffice-pdfimport
 
 # PgAdmin4, from: https://www.pgadmin.org/download/pgadmin-4-apt/
 curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
@@ -52,3 +85,16 @@ sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_relea
 apt-get install pgadmin4
 
 /usr/pgadmin4/bin/setup-web.sh
+
+# Habilitamos la documentación local
+cd /var/www/html
+ln -vs /usr/share/doc/postgresql-doc-13/html postgresql
+ln -vs /usr/share/doc/hyperspec .
+ln -vs /usr/share/gtk-doc/html/gdk2 .
+ln -vs /usr/share/gtk-doc/html/gtk2 .
+ln -vs /usr/share/doc/libgtk2.0-doc/faq gtk2-faq
+ln -vs /usr/share/doc/libgtk2.0-doc/tutorial gtk2-tutorial
+ln -vs /usr/share/gtk-doc/html/gdk3 .
+ln -vs /usr/share/gtk-doc/html/gtk3 .
+ln -vs /usr/share/doc/libgtkmm-2.4-doc/reference/html gtkmm2
+ln -vs /usr/share/doc/libgtkmm-3.0-doc/reference/html gtkmm3
